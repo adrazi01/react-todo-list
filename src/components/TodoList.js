836358a497de1f5255cd0d39c798f8ef641f77ce
adrazi01 <adrazi01@fesb.hr>
2021-10-06 18:58:05 +0200

    Third commit, listing functions

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,10 +3,14 @@ import TodoItem from "./TodoItem";
 
 export default class TodoList extends Component {
   render() {
+    const { items } = this.props;
     return (
       <ul className="list-group my-5">
         <h3 className="text-capitalize text-center">todo List</h3>
-        <TodoItem />
+        {items.map((item) => {
+          return <TodoItem key={item.id} title={item.title} />;
+        })}
+
         <button
           type="button"
           className="btn btn-danger w-100 text-capitalize mt-5"
